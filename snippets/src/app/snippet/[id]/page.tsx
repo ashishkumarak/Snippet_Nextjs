8f4faad68b94d956901643bcd12096a74a3cafdc
ashishkumarak <121762140+ashishkumarak@users.noreply.github.com>
2025-10-01 15:56:19 +0000

    show snippet of Dynamic Route

--- snippets/src/app/snippet/[id]/page.tsx
+++ snippets/src/app/snippet/[id]/page.tsx
@@ -1,16 +1,39 @@
+import { Button } from "@/components/ui/button";
+import { prismaClient } from "@/lib/prisma";
 import React from "react";
 
-const SnippetDetailPage = async ({ params,
-}: { params: Promise<{ id: string }>;}) => {
+// you can do like also :- async( {params} : {params:Promise<{id:string}>} )
+
+type SnippetDetailsProps = {
+  params: Promise<{ id: string }>
+}
+
+const SnippetDetailPage : React.FC<SnippetDetailsProps> = async ({ params}) => {
 
 
-  const id = (await params).id;
+  const id = parseInt((await params).id);
+  const snippet = await prismaClient.snippet.findUnique({
+    where : {
+      id,
+    }
+  });
 
+  if(!snippet) return <h1>Snippet not found</h1> // if no data in snippet var
 
 
   return (
-
-  <div>{id}</div>
+    <div className="flex flex-col gap-5">
+      <div className="flex items-center justify-between">
+        <h1 className="font-bold text-xl">{snippet.title} </h1>
+        <div className="flex items-center gap-2">
+          <Button>Edit</Button>
+          <Button variant={'destructive'}>Delete</Button>
+        </div>        
+      </div>
+      <pre className="p-3 bg-gray-200 rounded border-gray-200">
+        <code>{snippet.code}</code>
+      </pre>
+    </div>
 
 )
 };
