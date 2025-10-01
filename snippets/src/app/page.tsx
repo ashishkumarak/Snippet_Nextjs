6ae08512be8b15d5369e89645f586deeb527decb
ashishkumarak <121762140+ashishkumarak@users.noreply.github.com>
2025-10-01 12:29:08 +0000

    fetch and show first snippet in homepage

--- snippets/src/app/page.tsx
+++ snippets/src/app/page.tsx
@@ -1,16 +1,33 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { prismaClient } from "@/lib/prisma";
+import { INSPECT_MAX_BYTES } from "buffer";
+
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const snippets = await prismaClient.snippet.findMany();
+
   return (
     <div>
       <h1 className="font-bold text-4xl">Home</h1>
       <div className="flex items-center justify-between">
         <h1>Snippets</h1>
-        <Link href={"/snippet/new"}><Button>New</Button>
+        <Link href={"/snippet/new"}>
+          <Button>New</Button>
         </Link>
       </div>
+
+      {snippets.map((items) => (
+        <div
+          key={items.id}
+          className="flex items-center justify-between bg-gray-200 p-2 rounded-md my-2 "
+        >
+          <h1>{items.title}</h1>
+          <Link href={"/snippet/${items.id}"}>
+            <Button variant={"link"}>View</Button>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 }
