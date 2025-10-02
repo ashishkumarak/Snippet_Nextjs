ab96fdff6682c7967f668baa95e9b9b67e42e917
ashishkumarak <121762140+ashishkumarak@users.noreply.github.com>
2025-10-02 08:18:43 +0000

    complete Delete

--- snippets/src/actions/index.ts
+++ snippets/src/actions/index.ts
@@ -3,6 +3,7 @@
 import { prismaClient } from "@/lib/prisma";
 import { redirect } from "next/navigation";
 
+// For Saving the Snippet
 export const saveSnippet = async (id: number, code: string) => {
   await prismaClient.snippet.update({
     where: {
@@ -14,4 +15,16 @@ export const saveSnippet = async (id: number, code: string) => {
   });
 
   redirect(`/snippet/${id}`);
+}
+
+
+// For Deleting the snippet
+export const deleteSnippet = async(id:number) =>{
+  await prismaClient.snippet.delete({
+    where:{
+      id
+    }
+  });
+
+  redirect('/');
 }
--- snippets/src/app/snippet/[id]/page.tsx
+++ snippets/src/app/snippet/[id]/page.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { prismaClient } from "@/lib/prisma";
 import Link from "next/link";
 import React from "react";
+import * as actions from "@/actions";
 
 // you can do like also :- async( {params} : {params:Promise<{id:string}>} )
 
@@ -21,6 +22,7 @@ const SnippetDetailPage : React.FC<SnippetDetailsProps> = async ({ params}) => {
 
   if(!snippet) return <h1>Snippet not found</h1> // if no data in snippet var
 
+  const deleteSnippetActions = actions.deleteSnippet.bind(null,snippet.id);
 
   return (
     <div className="flex flex-col gap-5">
@@ -28,7 +30,11 @@ const SnippetDetailPage : React.FC<SnippetDetailsProps> = async ({ params}) => {
         <h1 className="font-bold text-xl">{snippet.title} </h1>
         <div className="flex items-center gap-2">
           <Link href={`/snippet/${snippet.id}/edit`}><Button>Edit</Button></Link>
-          <Button variant={'destructive'}>Delete</Button>
+
+          <form action={deleteSnippetActions}>
+            <Button variant={'destructive'} type="submit">Delete</Button>
+          </form>  
+
         </div>        
       </div>
       <pre className="p-3 bg-gray-200 rounded border-gray-200">
