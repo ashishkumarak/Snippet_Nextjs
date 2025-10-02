90f79767ef8d137012119f0726b7fa66e31d4920
ashishkumarak <121762140+ashishkumarak@users.noreply.github.com>
2025-10-02 09:34:06 +0000

    useActionState Hook for solve the error of putting empty title & code.

--- snippets/src/actions/index.ts
+++ snippets/src/actions/index.ts
@@ -28,3 +28,33 @@ export const deleteSnippet = async(id:number) =>{
 
   redirect('/');
 }
+
+// For Creating the new Snippet
+ export async function createSnippet(prevState:{message:string},formData:FormData){
+    // "use server" // Use ServerAction Directive // now it be removed/comment when added in server actions file.
+    const title = formData.get("title");
+    const code = formData.get("code");
+
+    // Now here checking the condition
+    // if(!title)
+    if(typeof title! == "string" || title.length < 4 ){
+      return {message:"Title is required and must atleast 4 char"}
+    }
+
+    if(typeof code! == "string" || code.length < 10 ){
+      return {message:"Code is required and must atleast 10 char"}
+    }
+
+
+
+    const snippet = await prismaClient.snippet.create({
+      data:{
+        title,
+        code
+      }
+    });
+    console.log("Created Snippet", snippet);
+
+    redirect("/"); // redirect to home page. 
+  }
+  
--- snippets/src/app/snippet/new/page.tsx
+++ snippets/src/app/snippet/new/page.tsx
@@ -1,33 +1,41 @@
-    
+"use client"
+
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Textarea } from '@/components/ui/textarea'
 import { prismaClient } from '@/lib/prisma'
 import { redirect } from 'next/navigation'
-import React from 'react'
+import React, { useActionState } from 'react'
+
+import * as actions from "@/actions";
 
 const CreateSnippetPage = () => {
 
-  async function createSnippet(formData:FormData){
-    "use server" // Use ServerAction Directive
-    const title = formData.get("title") as string;
-    const code = formData.get("code") as string;
+  const [formDatacoming , updatedActions] = useActionState(actions.createSnippet, {message:""});
 
-    const snippet = await prismaClient.snippet.create({
-      data:{
-        title,
-        code
-      }
-    });
-    console.log("Created Snippet", snippet);
 
-    redirect("/"); // redirect to home page. 
-  }
+
+  // async function createSnippet(formData:FormData){
+  //   "use server" // Use ServerAction Directive
+  //   const title = formData.get("title") as string;
+  //   const code = formData.get("code") as string;
+
+  //   const snippet = await prismaClient.snippet.create({
+  //     data:{
+  //       title,
+  //       code
+  //     }
+  //   });
+  //   console.log("Created Snippet", snippet);
+
+  //   redirect("/"); // redirect to home page. 
+  // }
+  // above code move at server action file with some code update .
   
  
   return (
-    <form action={createSnippet} >
+    <form action={updatedActions} >
       <div>
         <Label>Title</Label>
         <Input type='text' name='title' id='title'/>
@@ -36,6 +44,9 @@ const CreateSnippetPage = () => {
         <Label>Code</Label>
         <Textarea name='code' id='code'/>
       </div>
+
+      { formDatacoming.message && <div className='p-2 bg-red-300 border-2 border-red-600 mt-2'>{formDatacoming.message}</div> }
+      
       <Button type='submit' className='my-4'>New</Button>
     </form>
   )
